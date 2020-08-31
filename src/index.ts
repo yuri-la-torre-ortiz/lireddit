7f5ced3d655b16666a8cfdd51a6a44709f76a642
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-31 16:00:22 +0200

    Create & Read GraphQL database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import "reflect-metadata";
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import microConfig from "./mikro-orm.config";
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
 import {buildSchema} from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -15,16 +16,17 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
-        })
+        }),
+        context: () => ({ em: orm.em})
     });
 
     apolloServer.applyMiddleware({ app });
 
     app.listen(4000, () => {
         console.log('Server started on localhost:4000.')
-    })
+    });
 }
 
 main().catch(err => {
