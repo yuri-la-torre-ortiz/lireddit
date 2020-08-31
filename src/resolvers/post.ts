59bb9645fa433b878ad3b65fa3bcb37c35ddfefa
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-31 17:23:58 +0200

    Clean up unnecessary imports

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,7 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
-import { idText } from 'typescript';
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -12,7 +11,7 @@ export class PostResolver {
 
     @Query(() => Post, {nullable: true})
     post(
-        @Arg('id', () => Int) id: number,
+        @Arg('id') id: number,
         @Ctx() { em }: MyContext
     ): Promise<Post | null>  {
         return em.findOne(Post, { id });
