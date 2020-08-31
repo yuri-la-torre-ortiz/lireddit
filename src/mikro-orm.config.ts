01cd967073b5c1d511f4346d5eab7250d0acc8c4
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-31 18:39:24 +0200

    Register new users

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import {MikroORM} from "@mikro-orm/core";
 import path from 'path';
+import { User } from "./entities/User";
 
 console.log("dirname: ", __dirname);
 export default {
@@ -9,9 +10,9 @@ export default {
         path: path.join(__dirname, './migrations'), // path to the folder with migrations
         pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'lireddit',
     clientUrl: 'postgresql://postgres:postgres@localhost:5432/lireddit',
     type: 'postgresql',
     debug: !__prod__,
-} as Parameters<typeof MikroORM.init>[0];
+} as Parameters<typeof MikroORM.init>[0]; 
