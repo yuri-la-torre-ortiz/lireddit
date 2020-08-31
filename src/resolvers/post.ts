4a9c9f339a6376c29a84c962ad2c78c101a6bc3b
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-31 17:19:31 +0200

    Finish TypeGraphQL CRUD with Delete post feature

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -44,4 +44,12 @@ export class PostResolver {
         }
         return post;
     }
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg('id') id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<boolean>  {
+        await em.nativeDelete(Post, {id})
+        return true;
+    } 
 }
