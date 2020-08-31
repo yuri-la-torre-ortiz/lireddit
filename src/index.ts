2644d44486971220dbb33667ffab550962279dde
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-31 15:06:31 +0200

    Complete GraphQL Server setup

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,25 @@ import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from "./mikro-orm.config";
 import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
     const app = express();
-    app.get('/', (_, res) => {
-        //when ignorning a variable an '_' is standard practice
-        res.send('hello world');
-    })
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
     app.listen(4000, () => {
         console.log('Server started on localhost:4000.')
     })
